a0d04583d4ea3d7d53d92cd4b42c4df2f923c5e6
Brian Lawler <bplawler@gmail.com>
2015-08-25 17:35:15 -0700

    adding in API/impl classes so that outside apps have an entry point to call.

---
+++ src/main/java/TrackerApi.java
@@ -0,0 +1,6 @@
+package pirc.kpi;
+
+public interface TrackerApi {
+    String list(String path);
+    String status(String path);
+}
---
+++ src/main/scala/TrackerImpl.scala
@@ -0,0 +1,6 @@
+package pirc.kpi
+
+class TrackerImpl extends TrackerApi {
+  def list(x$1: String): String = ???
+  def status(x$1: String): String = ???
+}
